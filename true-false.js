997d24a3d36deea01bdc2b62c471baa5c497f173
MS MASKUR CHOWDHURY <chymaskur@gmail.com>
2021-02-02 09:20:51 +0300

    true false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//0 -false
+//false -false
+//"" -false
+//undefined -false
+//null -false
+//NaN -false
+
+//"0" -true
+// " " -true
+
+const age = 1;
+if(age > 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
